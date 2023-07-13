ee8dd78e65a686701464e7dfe5c11f4e2318397a
diorizqi404 <rizqiii.dio@gmail.com>
2023-07-13 21:50:15 +0700

    add content

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        secondary: '#172554',
+      },
+      fontFamily: {
+        'space-grotesk': ["'Space Grotesk', sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
